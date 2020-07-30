9848e6ae20c9055db4a5ffb3ea264ae957ce76d6
Tarun Jha <jhatarun16@gmail.com>
2020-07-30 20:05:14 +0530

    changes added

--- stack/nextGreaterElementLeft.cpp
+++ stack/nextGreaterElementLeft.cpp
@@ -1,19 +1,20 @@
-// next greater Element to the left inorder#include<bits/stdc++.h>
+// next greater Element to the left
+// previous greater element
+#include<bits/stdc++.h>
 using namespace std;
 
 // tc : O(n)
-vector<int> nextGreaterElement(int arr[], int n){
+vector<int> previousGreaterElement(int arr[], int n){
     vector<int> result(n, -1);
-    stack<int> s;
 
-    for(int i = 0; i < n; i++){
-        if(i == 0 || s.empty()){
+    stack<int> s;
+    for(int i = n-1; i >= 0; i--){
+        if(i == n-1 || s.empty()){
             s.push(i);
             continue;
         }
 
         while(!s.empty() && arr[i] > arr[s.top()]){
-            // cout << arr[i] << " ";
             result[s.top()] = arr[i];
             s.pop();
         }
@@ -25,13 +26,13 @@ vector<int> nextGreaterElement(int arr[], int n){
 }
 
 int main(){
-    // int arr[] = {1, 3, 2, 4};
-    // int arr[] = {4, 3, 2, 1};
-    int arr[] = {7, 8, 1, 4};
+    // int arr[] = {10, 4, 2, 20, 40, 12, 30};
+    // int arr[] = {10, 20, 30, 40};
+    int arr[] = {40, 30, 20, 10};
 
     int n = sizeof(arr)/sizeof(arr[0]);
 
-    for(auto i:nextGreaterElement(arr, n))
+    for(auto i:previousGreaterElement(arr, n))
         cout << i << " ";
     cout << endl;
 
---
+++ stack/stockSpan.cpp
@@ -0,0 +1,9 @@
+// 901. Online Stock Span
+#include <bits/stdc++.h>
+using namespace std;
+
+// Driver function
+int main(){
+
+    return 0;
+}
