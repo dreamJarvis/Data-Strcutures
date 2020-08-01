926c39428070c7cbedf3ced258751d662f7076b0
Tarun Jha <jhatarun16@gmail.com>
2020-08-01 16:41:53 +0530

    changes added

--- stack/containerWithMostWater.cpp
+++ stack/containerWithMostWater.cpp
@@ -1,1 +1,16 @@
-// leetcode : 11
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxArea(vector<int>& arr) {
+    int n = arr.size();
+
+    stack<int> s;
+
+    
+}
+
+// Driver function
+int main(){
+    vector<int> ({1,8,6,2,5,4,8,3,7});
+    return 0;
+}
