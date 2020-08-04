b438df8e826e0948967dc1933fc741b3111e9089
Tarun Jha <jhatarun16@gmail.com>
2020-08-04 18:22:17 +0530

    changes added

--- arrays/maxSlidingWindow.cpp
+++ arrays/maxSlidingWindow.cpp
@@ -4,70 +4,71 @@ using namespace std;
 
 // tc : O(n-k)
 // sc : O(k)
-// vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-//     int n = nums.size();
-//
-//     vector<int> result;
-//     set<pair<int, int>, greater<pair<int,int>>> s;
-//
-//     for(int i = 0; i < k; i++){
-//         s.insert(make_pair(nums[i], i));
-//     }
-//
-//     auto i = s.begin();
-//     result.push_back(i->first);
-//
-//     for(int i = 0, j = k; j < n; i++, j++){
-//         s.erase(make_pair(nums[i], i));
-//         s.insert(make_pair(nums[j], j));
-//         result.push_back(s.begin()->first);
-//     }
-//
-//     return result;
-// }
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    int n = nums.size();
+
+    vector<int> result;
+    set<pair<int, int>, greater<pair<int,int>>> s;
+
+    for(int i = 0; i < k; i++){
+        s.insert(make_pair(nums[i], i));
+    }
+
+    auto i = s.begin();
+    result.push_back(i->first);
+
+    for(int i = 0, j = k; j < n; i++, j++){
+        s.erase(make_pair(nums[i], i));
+        s.insert(make_pair(nums[j], j));
+        result.push_back(s.begin()->first);
+    }
+
+    return result;
+}
 
 // using deque
 vector<int> maxSlidingWindow(vector<int>& nums, int k) {
     int n = nums.size();
 
     vector<int> result;
-    deque<int> Qi;
-    int i;
+    deque<int> q;
 
-    int maxElement = INT_MIN, index=0;
-    for(i = 0; i < k; i++){
-        while(!Qi.empty() && nums[i] >= nums[Qi.back()])
-            Qi.pop_back();
-        Qi.push_back(i);
+    int i=0;
+    for(; i<k; i++){
+        while(!q.empty() && nums[i] >= nums[q.back()])
+            q.pop_back();
+        q.push_back(i);
     }
 
     for(; i < n; i++){
-        result.push_back(nums[Qi.front()]);
+        result.push_back(nums[q.front()]);
 
-        while(!Qi.empty() && Qi.front() <= i-k)
-            Qi.pop_front();
+        // elements should be in descending order
+        while(!q.empty() && nums[i] >= nums[q.back()])
+            q.pop_back();
 
-        while(!Qi.empty() && nums[i] >= nums[Qi.back()])
-            Qi.pop_back();
+        // element should be in the range
+        while(!q.empty() && q.front() <= i-k)
+            q.pop_front();
 
-        Qi.push_back(i);
+        q.push_back(i);
     }
 
-    result.push_back(nums[Qi.front()]);
+    result.push_back(nums[q.front()]);
 
     return result;
 }
 
 // Driver function
 int main(){
-    vector<int> arr({1,3,-1,-3,5,3,6,7});
-    int k = 3;
+    // vector<int> arr({1,3,-1,-3,5,3,6,7});
+    // int k = 3;
     // vector<int> arr({-7,-8,7,5,7,1,6,0});
     // int k = 4;
     // vector<int> arr({1, -1});
     // int k = 1;
-    // vector<int> arr({9,10,9,-7,-4,-8,2,-6});
-    // int k = 5;
+    vector<int> arr({9,10,9,-7,-4,-8,2,-6});
+    int k = 5;
 
     for(auto i:maxSlidingWindow(arr, k))
         cout << i << " ";
