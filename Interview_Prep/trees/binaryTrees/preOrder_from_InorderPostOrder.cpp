4ed7c6a4ff73f0edd62c1298605a8c8b2bd58c33
Tarun Jha <jhatarun16@gmail.com>
2020-08-05 22:36:35 +0530

    changes added

--- Interview_Prep/trees/binaryTrees/preOrder_from_InorderPostOrder.cpp
+++ Interview_Prep/trees/binaryTrees/preOrder_from_InorderPostOrder.cpp
@@ -5,20 +5,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int preIndex = 5;
+int preIndex = 0;
 
-// tc : O(n)
-// sc : O(n), for using hash-map
+// tc : O(n^2)
+// sc : 2*O(n), for using hash-map, stack
 void preorder( vector<int> &inorder, vector<int> &postorder,
                 unordered_map<int, int> &map, stack<int> &s, int left, int right)
 {
+    if(left > right)
+        return ;
 
+    int val = postorder[preIndex];
+    int index = map[postorder[preIndex--]];
+
+    preorder(inorder, postorder, map, s, index+1, right);       // right sub-tree
+    preorder(inorder, postorder, map, s, left, index-1);        // left sub-tree
+
+    s.push(val);                                                // root
 }
 
 void preorderUtil(vector<int> &inorder, vector<int> &postorder){
     int n = inorder.size();
+    preIndex = n - 1;
     unordered_map<int, int> mp;
     stack<int> s;
+
+    for(int i = 0; i < n; i++)
+        mp[inorder[i]] = i;
+
+    preorder(inorder, postorder, mp, s, 0, n-1);
+
+    while(!s.empty()){
+        cout << s.top() << " ";
+        s.pop();
+    }
 }
 
 // Driver function
