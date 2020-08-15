38d0fb0ed372bf3678c07c5a5bfdc42659aabf0b
Tarun Jha <jhatarun16@gmail.com>
2020-08-15 23:12:12 +0530

    changes added

--- Interview_Prep/trees/binaryTrees/problems/burnBinaryTree.cpp
+++ Interview_Prep/trees/binaryTrees/problems/burnBinaryTree.cpp
@@ -2,3 +2,11 @@
     Minimum time to burn a Tree starting from a Leaf node
     https://www.geeksforgeeks.org/minimum-time-to-burn-a-tree-starting-from-a-leaf-node/
 */
+#include <bits/stdc++.h>
+using namespace std;
+
+// Driver function
+int main(){
+
+    return 0;
+}
