52f7cfa98be75e71b27a31a3f8d15da2c7b45eda
Tarun Jha <jhatarun16@gmail.com>
2020-08-17 13:26:15 +0530

    changes added

--- Interview_Prep/trees/BST/implmentation/implementation.cpp
+++ Interview_Prep/trees/BST/implmentation/implementation.cpp
@@ -2,6 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Tree node
 struct TreeNode{
     int val;
     TreeNode *left, *right;
@@ -14,8 +15,7 @@ struct TreeNode{
 
 // insertion in tree
 TreeNode *insert(TreeNode *root, int key){
-    if(!root)
-        return new TreeNode(key);
+    if(!root)   return new TreeNode(key);
 
     // inserting accordingly to values
     if(key > root->val)                                 // if val is smaller than root
@@ -32,10 +32,12 @@ bool search(TreeNode *root, int k){
 
     if(root->val == k)
         return true;
-    else if(k < root->val)
-        return search(root->left, k);
-    else
-        return search(root->right, k);
+
+    bool leftSubtree = search(root->left, k);     // search for that element in the left subtree
+    bool rightSubtree = search(root->right, k);   // search for that element in the right subtree
+
+    // return true if present in either subtree
+    return (leftSubtree || rightSubtree);
 }
 
 // inorder trversal
@@ -47,21 +49,39 @@ void inorderTraversal(TreeNode *root){
     inorderTraversal(root->right);
 }
 
+// preOrder traversal
+void preOrderTraversal(TreeNode *root){
+    if(!root)   return ;
+
+    cout << root->val <<" ";
+    preOrderTraversal(root->left);
+    preOrderTraversal(root->right);
+}
+
+// post order traversals
+void postOrderTraversals(TreeNode *root){
+    if(!root)   return;
+
+    postOrderTraversals(root->left);
+    postOrderTraversals(root->right);
+    cout << root->val << " ";
+}
+
 // Driver function
 int main(){
-    // TreeNode *root = nullptr;
-    // root = insert(root, 50);
-    //
-    // insert(root, 30);
-    // insert(root, 20);
-    // insert(root, 40);
-    // insert(root, 70);
-    // insert(root, 60);
-    // insert(root, 80);
+    TreeNode *root = nullptr;
+    root = insert(root, 50);
 
-    // cout << search(root, 20) << endl;
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
 
-    // inorderTraversal(root);
+    cout << search(root, 180) << endl;
+
+    inorderTraversal(root);
 
 
     return 0;
